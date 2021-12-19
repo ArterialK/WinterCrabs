c184b7210ebca6784c29ca16b6f5ddd5c27b77c9
ArterialK <42286659+ArterialK@users.noreply.github.com>
2021-12-19 15:49:31 -0600

    Version preliminar

--- app/src/main/java/com/example/wintercrabs/loginMain.kt
+++ app/src/main/java/com/example/wintercrabs/loginMain.kt
@@ -1,11 +1,25 @@
 package com.example.wintercrabs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class loginMain : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+    fun Siguiente(view: android.view.View) {
+        val etUsuario = findViewById<TextView>(R.id.etUsuario)
+        val etContrasena = findViewById<TextView>(R.id.etContrasena)
+        if(etUsuario.text.isNotEmpty()){
+            if(etContrasena.text.isNotEmpty()){
+                val intent = Intent(this,ActivityMenu::class.java)
+                startActivity(intent)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/wintercrabs/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/wintercrabs/ui/slideshow/SlideshowFragment.kt
@@ -31,7 +31,7 @@ class SlideshowFragment : Fragment() {
         _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
+        val textView: TextView = binding.textAcerca
         slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
